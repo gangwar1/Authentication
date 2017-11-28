5b1a7805005a1b36efc95a343f7a7ff1c4014fb5
Lagvendra gangwar <gangwar.lagvendra6@gmail.com>
2017-11-29 00:25:51 +0530

    Add CreateUser

--- AspNetIdentity/AspNetIdentity.WebApi/Controllers/AccountController.cs
+++ AspNetIdentity/AspNetIdentity.WebApi/Controllers/AccountController.cs
@@ -17,12 +17,13 @@ using AspNetIdentity.WebApi.Models;
 using AspNetIdentity.WebApi.Providers;
 using AspNetIdentity.WebApi.Results;
 using AspNetIdentity.WebApi.Infrastructure;
+using System.Linq;
 
 namespace AspNetIdentity.WebApi.Controllers
 {
     [Authorize]
     [RoutePrefix("api/Account")]
-    public class AccountController : ApiController
+    public class AccountController : BaseApiController
     {
         private const string LocalLoginProvider = "Local";
         private ApplicationUserManager _userManager;
@@ -383,6 +384,70 @@ namespace AspNetIdentity.WebApi.Controllers
             }
 
             base.Dispose(disposing);
+        }
+
+
+        [Route("users")]
+        public IHttpActionResult GetUsers()
+        {
+            return Ok(this.AppUserManager.Users.ToList().Select(u => this.TheModelFactory.Create(u)));
+        }
+
+        [Route("user/{id:guid}", Name = "GetUserById")]
+        public async Task<IHttpActionResult> GetUser(string Id)
+        {
+            var user = await this.AppUserManager.FindByIdAsync(Id);
+
+            if (user != null)
+            {
+                return Ok(this.TheModelFactory.Create(user));
+            }
+
+            return NotFound();
+
+        }
+
+        [Route("user/{username}")]
+        public async Task<IHttpActionResult> GetUserByName(string username)
+        {
+            var user = await this.AppUserManager.FindByNameAsync(username);
+
+            if (user != null)
+            {
+                return Ok(this.TheModelFactory.Create(user));
+            }
+
+            return NotFound();
+
+        }
+        [Route("create")]
+        public async Task<IHttpActionResult> CreateUser(CreateUserBindingModel createUserModel)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+
+            var user = new ApplicationUser()
+            {
+                UserName = createUserModel.Username,
+                Email = createUserModel.Email,
+                FirstName = createUserModel.FirstName,
+                LastName = createUserModel.LastName,
+                Level = 3,
+                JoinDate = DateTime.Now.Date,
+            };
+
+            IdentityResult addUserResult = await this.AppUserManager.CreateAsync(user, createUserModel.Password);
+
+            if (!addUserResult.Succeeded)
+            {
+                return GetErrorResult(addUserResult);
+            }
+
+            Uri locationHeader = new Uri(Url.Link("GetUserById", new { id = user.Id }));
+
+            return Created(locationHeader, TheModelFactory.Create(user));
         }
 
         #region Helpers
