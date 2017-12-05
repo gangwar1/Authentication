d7275a6659d20a2a532b2a23a28bccfd9fb764b6
Lagvendra gangwar <gangwar.lagvendra6@gmail.com>
2017-12-05 21:35:13 +0530

    remove

--- AspNetIdentity/AspNetIdentity.WebApi/Areas/HelpPage/ModelDescriptions/SimpleTypeModelDescription.cs
+++
@@ -1,6 +0,0 @@
-namespace AspNetIdentity.WebApi.Areas.HelpPage.ModelDescriptions
-{
-    public class SimpleTypeModelDescription : ModelDescription
-    {
-    }
-}
--- AspNetIdentity/AspNetIdentity.WebApi/Global.asax
+++
@@ -1,1 +0,0 @@
-﻿<%@ Application Codebehind="Global.asax.cs" Inherits="AspNetIdentity.WebApi.WebApiApplication" Language="C#" %>
